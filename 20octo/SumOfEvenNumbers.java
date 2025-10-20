8a82921b2db6b8315752720c73f02d049d4f3bc9
ManishNagrale <manishnagrale22@gmail.com>
2025-10-20 17:54:43 +0530

    20oct commit

---
+++ 20octo/SumOfEvenNumbers.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class SumOfEvenNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value of n: ");
+        int n = sc.nextInt();
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += 2 * i;
+        }
+         System.out.println("Sum of first " + n + " even numbers is: " + sum);
+    }
+}
